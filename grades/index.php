42c9c8053db8692188e5e27ef2ca72993cabd172
Sibren Talens <sibrentalens@gmail.com>
2015-05-01 21:47:13 +0200

    asdf

---
+++ grades/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'inc/head.php'; ?>
+<body>
+<div id="wrapper">
+<?php include 'inc/header.php'; ?>
+
+<?php include 'inc/footer.php'; ?>
+</div>
+</body>
+</html>
