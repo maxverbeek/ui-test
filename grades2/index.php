da625e1aed6b30ff6bee3982fd55aa31d33efd30
Max Verbeek <m4xv3rb33k@gmail.com>
2015-05-11 19:56:10 +0200

    Heb iets gemaakt dat op die van jouw lijkt, deze schaalt wel mee met mobile idk hoe lol

---
+++ grades2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Grades</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header class="main-header">
+		<nav>
+			<div class="icons center">
+				<div class="icon dashboard arrow">Dashboard</div>
+				<div class="icon grades">Grades</div>
+				<div class="icon inbox">Inbox</div>
+				<div class="icon profile">Profile</div>
+				<div class="icon settings">Settings</div>
+			</div>
+		</nav>
+	</header>
+
+	<div class="content center">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro sed, sunt nam sapiente voluptatum quidem, magnam impedit, unde aut numquam, eius ab. Eius, optio autem est velit debitis nesciunt illo.
+	</div>
+</body>
+</html>
