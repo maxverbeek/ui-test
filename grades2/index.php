87e2c2d8d37117c513884f84db8b36e4c6f151b3
Max Verbeek <m4xv3rb33k@gmail.com>
2015-05-13 19:25:51 +0200

    Heb de site wat meer kleur gegeven.

--- grades2/index.php
+++ grades2/index.php
@@ -24,5 +24,44 @@
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro sed, sunt nam sapiente voluptatum quidem, magnam impedit, unde aut numquam, eius ab. Eius, optio autem est velit debitis nesciunt illo.</p>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro sed, sunt nam sapiente voluptatum quidem, magnam impedit, unde aut numquam, eius ab. Eius, optio autem est velit debitis nesciunt illo.</p>
 	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>
+		$(function ()
+		{
+			var x, y;
+
+			var mlg = $('#jajaja');
+
+			var colors = ['deeppink', 'hotpink', 'purple', 'violet', 'deeppink', 'deeppink'];
+
+			$(document).on('mousemove', function (e)
+			{
+				x = e.pageX;
+				y = e.pageY;
+			});
+
+			var stars = [];
+
+			var range = 50;
+			setInterval(function ()
+			{
+				if (stars.length > 10)
+				{
+					var remove = stars.shift();
+					remove.parentElement.removeChild(remove);
+				}
+
+				var color = colors[Math.floor(Math.random() * colors.length)];
+				var top = (Math.random() - 0.5) * 50 + y;
+				var left = (Math.random() - 0.5) * 50 + x;
+				var img = $('<img src="img/kutsterren/'+ color +'.svg" style="position: fixed; top: '+ top +'px; left: '+ left +'px;" width="50px" height="50px" title="jajajaja">').get(0);
+
+			stars.push(img);
+			document.body.appendChild(img);
+			}, 25);
+		});
+	</script>
+	</script>
 </body>
 </html>
